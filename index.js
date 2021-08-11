3c82d799d488b1ae1a54587344faaff294127dfd
dayojohn19 <dayo_john16@yahoo.com>
2021-08-11 21:09:23 +0800

    webpage_zuit

---
+++ index.js
@@ -0,0 +1,6 @@
+u_name = prompt('To continue Please tell me your First Name:', 'visitor');
+
+
+document.addEventListener("DOMContentLoaded", ()=> {
+    document.querySelector("#u_name").append(u_name);
+})
